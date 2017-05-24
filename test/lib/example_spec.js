ef22c6a668119ef1b3aefffd4d99c2cd0bc2a066
Wender Freese <wender.jean@gmail.com>
2017-05-24 14:15:27 -0300

    Refactoring code

--- test/lib/example_spec.js
+++ test/lib/example_spec.js
@@ -4,17 +4,21 @@ const Example = require('../../lib/example');
 
 describe('Example', () => {
 
+    let wrapper;
+
     describe('#isMissingVariables', () => {
+
+        beforeEach((done) => {
+
+            wrapper = () => Example.isMissingVariables(".env.sample");
+            done();
+        });
 
         context('when .env.example does not exists', () => {
 
             it('should throw an exception', (done) => {
 
-                try {
-                    Example.isMissingVariables(".env.sample");
-                } catch(e) {
-                    expect(e.code).to.equal("ENOENT");
-                }
+                expect(wrapper).to.throw(Error);
                 done();
             });
         });
